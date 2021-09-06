ada656d6d509b493d4bcdca1d01cdc7dc002c630
Koushik Chatterjee <koushik350@gmail.com>
2021-09-06 23:38:51 +0530

    📦 NEW: Web scrapping

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ const config: PlaywrightTestConfig = {
     },
     // timeout: 10000,
     // grep: [new RegExp("@smoke"), new RegExp("@reg")],
-    testMatch: ["mobile.test.ts"],
+    testMatch: ["amazonprice.test.ts"],
     retries: 0,
     reporter: [["dot"], ["json", { outputFile: "test-result.json" }],
     ['experimental-allure-playwright']],
---
+++ webscraping/amazonprice.test.ts
@@ -0,0 +1,45 @@
+const url = "https://www.amazon.in/Maono-AU-400-Lavalier-Microphone-Black/dp/B07JF9B592/ref=sr_1_4?dchild=1&keywords=moana+mic&qid=1630822326&sr=8-4";
+
+import { test } from "@playwright/test";
+
+import * as auth from "./auth.json"
+
+const nodemailer = require("nodemailer");
+test("Amazon Price Drop Notification", async ({ page }) => {
+
+    // Navigate to the Amazon product page
+    await page.goto(url);
+    // get the price of the product 
+    const price = await page.$eval("#priceblock_ourprice", el => el.textContent);
+    // remove the currency symbol and the comma from the price
+    const currentPrice = price?.replace("₹", '').split(".")[0];
+    console.log(currentPrice);
+
+    // send the email using nodemailer
+    if (Number(currentPrice) < 400) {
+        sendEmailNotification(currentPrice);
+    }
+
+})
+
+function sendEmailNotification(currentPrice: string | undefined) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: auth.email,
+            pass: auth.password
+        }
+    });
+    transporter.sendMail({
+        from: auth.email,
+        to: 'koushik350@gmail.com',
+        subject: 'Amazon Price Drop Notification',
+        html: `<p>The price of the product has dropped to ${currentPrice}</p><a href="${url}">click to open</a>`
+    }, (err: any, info: any) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(info);
+        }
+    });
+}
