cae7e79cc93f8009372ab12b6241eb50340846fe
Koushik Chatterjee <koushik350@gmail.com>
2022-05-04 14:50:53 +0530

    👌 IMPROVE: Browser Maxi

--- advanceSelect/bootstrap.test.ts
+++ advanceSelect/bootstrap.test.ts
@@ -1,6 +1,12 @@
 // https://zetcode.com/javascript/regex/ - reference to learn Regex
 import { Page, test } from "@playwright/test";
 
+test.use({
+    viewport: {
+        height: 824,
+        width: 1536
+    }
+})
 test("Select from bootstrap dropdown", async ({ page }) => {
 
     await page.goto("file:///Y:/my-code-base/Playwright-Test-Runner/advanceSelect/index.html");
@@ -8,13 +14,13 @@ test("Select from bootstrap dropdown", async ({ page }) => {
     //     .selectOption({
     //         label: "Playwright"
     //     })
-
-    await page.locator(".filter-option").click();
     await selectOption(page, /^Playwright$/g);
     await selectOption(page, /^Cypress$/g);
+    await page.waitForTimeout(5000)
 })
 
 async function selectOption(page: Page, data: RegExp) {
+    await page.locator(".filter-option").click();
     await page.locator(".dropdown-menu")
         .locator("li", {
             has: page.locator("a span"),
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,9 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 
 const config: PlaywrightTestConfig = {
+
     use: {
+        viewport: null,
         headless: !true,
         browserName: "chromium",
         screenshot: "on",
@@ -17,16 +19,18 @@ const config: PlaywrightTestConfig = {
         // contextOptions: {
         //     permissions: ["clipboard-read"]
         // }
-        // launchOptions: {
-        //     logger: {
-        //         isEnabled: (name, severity) => true,
-        //         log: (name, severity, message, args) => console.log(name, severity)
-        // }
-        // }
+        ,
+        launchOptions: {
+            args: ["--start-maximized"],
+            logger: {
+                isEnabled: (name, severity) => true,
+                log: (name, severity, message, args) => console.log(name, severity)
+            }
+        }
     },
     // timeout: 60000,
     // grep: [new RegExp("@smoke"), new RegExp("@reg")],
-    // testMatch: ["customWaits\\waitForTextChange.ts"],
+    testMatch: ["advanceSelect\\bootstrap.test.ts"],
     retries: 0,
     // reporter: "./customReport/myReporter.ts"
     reporter: [
